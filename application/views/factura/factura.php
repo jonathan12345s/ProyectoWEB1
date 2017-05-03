51364de5eda4dddf90f32719975a5f2042c7f44d
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-05-02 22:06:51 -0600

    genera numero altelnorio para el codigo factura

--- application/views/factura/factura.php
+++ application/views/factura/factura.php
@@ -63,9 +63,27 @@
 
 <form class="form-inline">
   <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
-  </div>
+
+      <label for="email">Email address:</label>
+
+
+<?php
+
+$aleatorio=rand(1, 100);
+
+echo "El numero aleatorio es: "."$aleatorio";
+
+
+?>
+
+    <input type="text " value= '<?php echo $aleatorio; ?>'  class="form-control" id="email">
+
+
+
+
+ </div>
+
+
   <div class="form-group">
     <label for="pwd">Password:</label>
     <input type="password" class="form-control" id="pwd">
